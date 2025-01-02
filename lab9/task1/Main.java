bbfb4c563d20a273b42c0606c64544231a73a983
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2025-01-02 21:08:40 +0100

    Add lab9/task1

--- lab9/task1/Main.java
+++ lab9/task1/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            NullPointerExceptionThrower.throwNullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println("Program crashed: a fatal exception occurred: " + e.getMessage());
+            e.printStackTrace();
+            System.out.println(e.toString());
+        }
     }
 }
---
+++ lab9/task1/NullPointerExceptionThrower.java
@@ -0,0 +1,5 @@
+public class NullPointerExceptionThrower {
+    public static void throwNullPointerException() throws NullPointerException {
+        throw new NullPointerException("keyboard was not found or is null, press any key to continue");
+    }
+}
