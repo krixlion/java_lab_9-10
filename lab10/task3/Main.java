4debc11790cdfda22cf468bcac3e653ac285091d
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2025-01-02 22:15:38 +0100

    Add lab10/task3

--- lab10/task3/Main.java
+++ lab10/task3/Main.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+                BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"))) {
+            String line = reader.readLine();
+
+            while (line != null) {
+                writer.write(line.replace(' ', '-'));
+                writer.newLine();
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
